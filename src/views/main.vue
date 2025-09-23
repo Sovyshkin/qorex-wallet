37e5e837ec5a179fc90317efc40eb3fa36a1530a
Sovyshkin <zankov.vadik@yandex.ru>
2025-09-23 13:09:32 +0500

    debug

--- src/views/main.vue
+++ src/views/main.vue
@@ -25,6 +25,7 @@ onMounted(async () => {
 <template>
   <div class="wrapper">
     <header>
+      {{ walletStore.userTg }}
       <div class="user">
         <div class="wrap-avatar">
           <img :src="walletStore.userTg.photo_url" alt="" />
